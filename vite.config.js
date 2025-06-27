bfa55cb5971654c910414f08c323210358e0d531
abhi-zero <abhi.0zero@gmail.com>
2025-06-27 22:25:38 +0530

    fix: update base path in Vite configuration for correct deployment

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/your-repo-name/', // Replace 'your-repo-name' with the actual name of your GitHub repository
+  base: '/Portfolio/', // Replace 'your-repo-name' with the actual name of your GitHub repository
 });
