415b4cba02dd13dfcdd10e53a1d4dbd3499af13d
abhi-zero <abhi.0zero@gmail.com>
2025-06-27 22:18:10 +0530

    chore: add initial Vite configuration file

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/your-repo-name/', // Replace 'your-repo-name' with the actual name of your GitHub repository
+});
